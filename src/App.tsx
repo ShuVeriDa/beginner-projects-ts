aeb6acb2feb10cf3cd2993dcc548987fcc28b468
ShuVeriDa <bashtarov@outlook.com>
2022-10-09 17:34:31 +0300

    added the ability increment and decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import './index.scss';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const onClickIncrement = () => {
+    setCount(count + 1)
+  }
+  const onClickDecrement = () => {
+    setCount(count - 1)
+  }
    return (
      <div className="App">
        <div>
          <h2>Счетчик:</h2>
-         <h1>0</h1>
-         <button className="minus">- Минус</button>
-         <button className="plus">Плюс +</button>
+         <h1>{count}</h1>
+         <button className="minus" onClick={onClickDecrement}  disabled={count === 0}>- Минус</button>
+         <button className="plus" onClick={onClickIncrement}>Плюс +</button>
        </div>
      </div>
    );
